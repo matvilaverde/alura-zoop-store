ba8cbd0b600e5ef89f3e514a8b3377a20fda3a98
Charleston Campos <charlinhocjc@gmail.com>
2024-02-13 09:08:13 -0300

    feat: project finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './shared/components/header/header.component';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [
+      HeaderComponent,
+      RouterOutlet
+    ]
 })
-export class AppComponent {
-  title = '3664-zoop-store';
-}
+export class AppComponent {}
