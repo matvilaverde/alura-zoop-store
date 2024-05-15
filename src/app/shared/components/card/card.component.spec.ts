7e477c17591512dd29ecaea9ccc12712c63ce46f
Mateus Vilaverde <matvilaverde@casuldev.com>
2024-05-14 21:56:00 -0300

    test dumb card component template

--- src/app/shared/components/card/card.component.spec.ts
+++ src/app/shared/components/card/card.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CardComponent } from './card.component';
+import { Product } from '../../../types/product.inteface';
+import { By } from '@angular/platform-browser';
 
 describe('CardComponent', () => {
   let component: CardComponent;
@@ -19,5 +21,29 @@ describe('CardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should render product template', () => {
+    const product: Product = {
+      id: 1,
+      title: 'iPhone 15',
+      price: '1000',
+      category: 'electronic',
+      description: 'smartphone',
+      image: 'src/assets/image.png'
+    }
+
+    component.product = product;
+    fixture.detectChanges();
+
+    const productImg = fixture.debugElement.query(By.css('img')).nativeElement;
+    const productTitle = fixture.debugElement.query(By.css('h2')).nativeElement;
+    const productDescription = fixture.debugElement.query(By.css('p')).nativeElement;
+    const productPrice = fixture.debugElement.query(By.css('h3')).nativeElement;
+
+    expect(productImg.src).toContain(product.image);
+    expect(productTitle.textContent).toContain(product.title);
+    expect(productDescription.textContent).toContain(product.description);
+    expect(productPrice.textContent).toContain(product.price);
   });
 });
