911046a76c1a807615b6a586e518f551f8282f14
Mateus Vilaverde <matvilaverde@casuldev.com>
2024-05-20 22:11:18 -0300

    fix base create-product test setup

--- src/app/pages/manage-products/create-product/create-product.component.spec.ts
+++ src/app/pages/manage-products/create-product/create-product.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { CommonModule } from '@angular/common';
 import { CreateProductComponent } from './create-product.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('CreateProductComponent', () => {
   let component: CreateProductComponent;
@@ -8,7 +18,22 @@ describe('CreateProductComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CreateProductComponent]
+      imports: [
+        BrowserAnimationsModule,
+        CommonModule,
+        CreateProductComponent,
+        HttpClientTestingModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatInputModule,
+        MatSelectModule,
+        ReactiveFormsModule,
+      ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: {} }
+      ]
     })
     .compileComponents();
     
